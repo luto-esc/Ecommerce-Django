525c15e854ad658c7d8e72e9a8f3f0e00a9d6d7a
luto-esc <luciomatiasescalante1@gmail.com>
2026-01-23 15:10:15 -0300

    opinion like en trabajo

--- apps/reactions/views.py
+++ apps/reactions/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from apps.products.models import Product
-from .models import Like, DisLike
+from apps.opinons.models import ProductOpinion
+from .models import Like, DisLike, OpinionLike, OpinionDisLike
 from django.contrib.auth.decorators import login_required
 
 #------------PRODUCT REACTION---------
@@ -22,6 +23,54 @@ def product_reaction(request, pk):
         #Comprueba si existe en la tabla
         have_like = Like.objects.filter(product=product,author=user).exists()
         have_dislike = DisLike.objects.filter(product=product,author=user).exists()
+
+        #Si el input es like
+        if action == 'like':
+
+            #Si existe en la tabla dislike lo elimina
+            if have_dislike:
+                DisLike.objects.filter(product=product,author=user).delete()
+
+            #Si existe en la tabla like
+            if have_like:
+                Like.objects.filter(product=product,author=user).delete()
+            else:
+                #Si no lo crea
+                Like.objects.create(product=product, author=user)
+         
+         #Si el input es dislike    
+        elif action == 'dislike':
+            
+            #Si existe en la tabla like lo elimina
+            if have_like:
+                Like.objects.filter(product=product,author=user).delete()
+
+            #Si existe en la tabla dislike lo elimina
+            if have_dislike:
+                DisLike.objects.filter(product=product,author=user).delete()
+            else:
+                #Sino lo crea
+                DisLike.objects.create(product=product, author=user)
+
+    return HttpResponseRedirect(reverse_lazy('products:path_product_detail', kwargs = {'pk':pk}))
+
+#------------OPINION REACTION---------
+@login_required
+def opinion_reaction(request, pk):
+    #Cargamos el formulario
+    if request.method == 'POST':
+        #Tomamos el producto
+        opinion = ProductOpinion.objects.get(pk=pk)
+
+        #Tomamos el usuario
+        user = request.user
+
+        #Según el botón que apretó
+        action = request.POST.get('action')
+
+        #Comprueba si existe en la tabla
+        have_like = OpinionLike.objects.filter(product=product,author=user).exists()
+        have_dislike = OpinionDisLike.objects.filter(product=product,author=user).exists()
 
         #Si el input es like
         if action == 'like':
