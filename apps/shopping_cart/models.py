df47d8deed50dc37c2f198d43c84199e93651b79
luto-esc <luciomatiasescalante1@gmail.com>
2026-01-25 15:09:11 -0300

    mas de shopping cart

--- apps/shopping_cart/models.py
+++ apps/shopping_cart/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from apps.products.models import Product
 from django.contrib.auth.models import User
 
 
 class ShoppingCart(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	products = models.ManyToManyField(Product, blank=True)
 
 	def __str__(self):
-		return self.user
+		return f"Shopping Cart of {self.user.username}"
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import UserRegisterForm
+from apps.shopping_cart.models import ShoppingCart
 
 class UserRegister(CreateView):
 	template_name = 'users/user_register.html'
 	form_class = UserRegisterForm
 	success_url = reverse_lazy('users:path_login')
+
+	def form_valid(self)
+		user = form.save()
+		ShoppingCart.objects.get_or_create(user=user)
+
+		return super().form_valid(form)
