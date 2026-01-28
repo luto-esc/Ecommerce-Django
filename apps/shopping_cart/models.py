09b7b5cdc5eac66fae46ccc7b1058b5099acaef4
luto-esc <luciomatiasescalante1@gmail.com>
2026-01-28 11:40:13 -0300

    actualizacion del carrito de compras

--- apps/shopping_cart/models.py
+++ apps/shopping_cart/models.py
@@ -5,7 +5,16 @@ from django.contrib.auth.models import User
 
 class ShoppingCart(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	products = models.ManyToManyField(Product, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return f"Shopping Cart of {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(ShoppingCart, related_name='items' , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
+
